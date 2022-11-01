b10d4bce0c4b80fb931db5ae89658c5fd0bc6b44
markhgr <hagerm@oregonstate.edu>
2022-10-31 20:33:35 -0700

    testing datetime

--- app.py
+++ app.py
@@ -9,17 +9,17 @@ https://www.digitalocean.com/community/tutorials/processing-incoming-request-dat
 """
 from flask import Flask, render_template, request
 import json  # used to open timestamp arrays
-import time  # used to calculate time difference
+from datetime import datetime  # used to calculate time difference
 
 app = Flask(__name__)
 
 @app.route("/", methods = ['POST', 'GET'])
 def main():
     if request.method == 'POST':
-        start_time = request.form['start_time']
-        end_time = request.form['end_time']
+        start_time = datetime(request.form['start_time'])
+        end_time = datetime(request.form['end_time'])
 
-        return render_template('app.html', message="OK!")
+        return render_template('app.html', start_time = start_time, end_time = end_time)
     return render_template('app.html')
 
 @app.route("/calculate_time", methods = ['POST'])
